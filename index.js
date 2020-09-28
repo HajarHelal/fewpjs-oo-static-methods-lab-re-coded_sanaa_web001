68b7563d3b7555b49902a7ee826f2488b81a5cd5
Hajar Helal <hajerhelal44@gmail.com>
2020-09-28 20:12:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ class Formatter {
   static capitalize(stri){
     return stri.charAt( 0 ).toUpperCase()+ stri.slice( 1 )
   }
-  //add static methods here
+  static sanitize( string ) {
+      return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+    }
 }
