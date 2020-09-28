e696d3e94a40e6fc296987fdaf2bf6d5a64d8f15
Hajar Helal <hajerhelal44@gmail.com>
2020-09-28 20:18:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 class Formatter {
-  static capitalize(stri){
-    return stri.charAt( 0 ).toUpperCase()+ stri.slice( 1 )
+  static capitalize( string ) {
+    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
   }
-  static sanitize(stri) {
-      return string.replace( /[^A-Za-z0-9 '-]/g, '' )
-    }
 
-    static titleize(sentence) {
-  let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
-  let result = [];
-  let arrayOfWords = sentence.split( " " )
-  for ( let n = 0; n < arrayOfWords.length; n++ ) {
-    if ( n == 0 ) {
-      result.push( this.capitalize( arrayOfWords[ n ] ) )
-    }
-    else {
-      if ( exceptions.includes( arrayOfWords[ n ] ) ) {
-        result.push( arrayOfWords[ n ] )
-      } else {
+  static sanitize( string ) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize( sentence ) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let result = [];
+    let arrayOfWords = sentence.split( " " )
+    for ( let n = 0; n < arrayOfWords.length; n++ ) {
+      if ( n == 0 ) {
         result.push( this.capitalize( arrayOfWords[ n ] ) )
+      } else {
+        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
+          result.push( arrayOfWords[ n ] )
+        } else {
+          result.push( this.capitalize( arrayOfWords[ n ] ) )
+        }
       }
+
     }
-
+    return result.join( " " );
   }
-  return result;
-}
-
 }
